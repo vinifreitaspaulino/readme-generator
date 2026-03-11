b99a5bba2038ff930a9ff79f5db563336583fce1
vinifreitaspaulino <100056453+vinifreitaspaulino@users.noreply.github.com>
2026-03-11 18:01:39 -0300

    feat: add --verbose flag and default output path

--- greadme/cli.py
+++ greadme/cli.py
@@ -1,6 +1,10 @@
 import argparse
 from pathlib import Path
 
+def log(msg, verbose):
+    if verbose:
+        print(msg)
+
 def main():
     parser = argparse.ArgumentParser(
         prog="greadme",
@@ -10,9 +14,15 @@ def main():
     parser.add_argument("path", type=Path, help="Path to the project")
     parser.add_argument("--lang", "-l",  choices=["en","pt"], default="en", help="README language (default: pt)")
     parser.add_argument("--output", "-o", type=Path, default=None, help="Output path (default: <project>/README.md)")
+    parser.add_argument("--verbose", "-v", action="store_true")
     
     args = parser.parse_args()
-    print(f"Projeto: {args.path}, idioma: {args.lang}, saída: {args.output}")  # teste
+    print("Rodando")  # teste
+
+    if args.output is None:
+        args.output = args.path / "README.md"
+
+    log(f"Projeto: {args.path}, idioma: {args.lang}, saída: {args.output}", args.verbose)
 
 if __name__ == "__main__":
     main()
